67bdf3e589194afb8d13ca14af0a61559bfea3cb
ThifannyS <127845293+ThifannyS@users.noreply.github.com>
2024-08-29 01:29:43 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+let anoAtual = 2024; 
+const nomeCompleto = "Thifanny";
+let idade = 17;
+const cursoFaculdade = "Psicologia";
+let cidadeAtual = "São José dos Pinhais";
+
+console.log(`ola eu sou a ${nomeCompleto} eu tenho ${idade} anos, moro em ${cidadeAtual} quero ano que vem estar cursando ${cursoFaculdade}`);
+
+anoAtual = 2025;
+idade = 18;
+cidadeAtual = "Curitiba";
+
+console.log(anoAtual);
